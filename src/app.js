5ea9cdcaba0b3900bc118b9356e490c2aad581aa
Ranj1 <ranjanatiwari1996@gmail.com>
2025-09-12 18:06:28 +0530

    Exploring Routing

--- src/app.js
+++ src/app.js
@@ -2,25 +2,49 @@ const express = require('express');
 
 const app = express();
 
+app.use('/user',(req,res) => {
+    res.send("ha ha ha");
+})
 
+app.get('/user',(req,res) => { 
 
-app.use('/hello',(req,res) => {
+    res.send({'firstName': "Ranjana","LastName": "Tiwari"})
+});
 
-    res.send("Hello from the server ranjana ");
+app.post('/user',(req,res) => { 
+
+    console.log("Data saved Successfully");
+    res.send("Data saved Successfully");
 
 });
 
-app.use('/namaste',(req,res) => {
+app.delete('/user',(req,res) => { 
 
-     res.send("Namaste from the server");
-
+    console.log("Data deleted Successfully");
+    res.send("Data deleted Successfully");
 
 });
-app.use('/',(req,res) => {
+
+app.patch('/user',(req,res) => { 
+
+    console.log("Data patched Successfully");
+    res.send("Data patched Successfully");
+
+});
+
+app.put('/user',(req,res) => { 
+
+    console.log("Data put Successfully");
+    res.send("Data put Successfully");
+
+});
+
+app.use('/test',(req,res) => {
 
      res.send("Home from the server");
 
 });
+
 
 
 app.listen(3004,() => {
